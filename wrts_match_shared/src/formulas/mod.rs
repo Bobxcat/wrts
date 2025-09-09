58fb707b4c2bd5111a3dedac4b85716387e70f4c
Ryan <ryan@rscheme.org>
2025-09-09 02:42:33 -0500

    Unfinished changes

--- wrts_match_shared/src/formulas/mod.rs
+++ wrts_match_shared/src/formulas/mod.rs
@@ -1,5 +1,7 @@
 use glam::*;
 
+use crate::ship_template::ShipTemplateId;
+
 /// Returns whether or not `v` is within the sweep from `from` to `to`,
 /// rotating clockwise
 ///
@@ -20,5 +22,27 @@ pub struct GunRangeCalc {
 impl GunRangeCalc {
     pub fn run(self) -> f32 {
         self.base_range
+    }
+}
+
+pub struct DamageCalcRes {
+    pub damage_dealt: f64,
+    //
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct DamageCalc {
+    pub ship: ShipTemplateId,
+    pub ship_pos: Vec3,
+    pub ship_rot: Quat,
+    pub projectile_base_damage: f64,
+    pub projectile_vel: Vec3,
+    pub intersection_pos: Vec3,
+    //
+}
+
+impl DamageCalc {
+    pub fn run(self) -> f64 {
+        self.projectile_base_damage
     }
 }
